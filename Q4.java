c352cfb046c8726e9d0283971e37ddfeb28bcbfe
saifullahkhanjadoon <skjadoon405@gmail.com>
2025-09-17 11:04:54 +0500

    Q4.java

--- Q4.java
+++ Q4.java
@@ -1,3 +1,12 @@
+// Q4. Write a menu driven program using do-while loop that inputs a value and a type of conversion. The program should output the value after conversion. The program should include the following conversions. Main Menu
+//  [1]. Convert Centimeter into Inches (1 inch = 2.54 cm)
+//  [2]. Liters into Gallons (1 gallon = 3.785 ltrs)
+//  [3]. Kilometers into Miles (1 mile = 1.609 km)
+//  [4]. Kilograms into Pounds (1 pound = .4536 kg)
+//  [5]. Exit
+//  My choice: Make sure the program accepts only valid choices for the type of conversion to perform
+
+
 import java.util.Scanner;
 public class Q4{
 
@@ -89,3 +98,4 @@ public class Q4{
 		while(choice.equals("y"));
 	}
 		}
+
